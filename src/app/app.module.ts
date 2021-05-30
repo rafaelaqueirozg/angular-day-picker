41f6ae7788cd9c91ac347e1822f0b81cb4cff2d7
Rafaela Queiroz <rqueiroz2108@gmail.com>
2021-05-30 01:10:58 -0300

    :sparkles: Feature (DayPickerComponent)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { DayPickerComponent } from "./day-picker/day-picker.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DayPickerComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/day-picker/day-picker.component.ts
@@ -0,0 +1,115 @@
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+
+interface DaysModel {
+  day: number;
+  selected?: boolean;
+  disabled?: boolean;
+}
+
+interface Range {
+  initialDay: number;
+  finalDay: number;
+}
+
+export interface SelectedDaysEmitter {
+  selectedDays: number[];
+  humanizedSelectedDays: string;
+}
+
+@Component({
+  selector: "day-picker",
+  templateUrl: "./day-picker.component.html",
+  styleUrls: ["./day-picker.component.scss"],
+})
+export class DayPickerComponent implements OnInit {
+  @Input()
+  range!: Range;
+
+  @Input()
+  maxSelectableDays!: number;
+
+  @Input()
+  disabledDays: number[] = [];
+
+  @Input()
+  required: boolean = false;
+
+  @Input()
+  label: string = "Month days";
+
+  @Output()
+  selectedDaysEmitter: EventEmitter<SelectedDaysEmitter> = new EventEmitter();
+
+  selectedDays: number[] = [];
+
+  days: DaysModel[] = [];
+
+  isShowingDayPicker = false;
+
+  closedWithoutSelect = false;
+
+  get isInvalid(): boolean {
+    return this.required && this.closedWithoutSelect;
+  }
+
+  get humanizedSelectedDays(): string {
+    let days = "";
+    const orderedDays = this.selectedDays.sort((a, b) => a - b);
+
+    const penultimateDay = orderedDays.length - 2;
+    const lastDay = orderedDays.length - 1;
+
+    for (let index = 0; index < orderedDays.length; index++) {
+      days += ` ${orderedDays[index]}${
+        index === penultimateDay ? " and " : index === lastDay ? "" : ", "
+      }`;
+    }
+
+    return `Every day ${days}`;
+  }
+
+  constructor() {}
+
+  toggleDayPicker(): void {
+    this.isShowingDayPicker = !this.isShowingDayPicker;
+
+    if (!this.closedWithoutSelect)
+      this.closedWithoutSelect =
+        !this.isShowingDayPicker && !this.selectedDays.length;
+  }
+
+  selectDay(day: number): void {
+    const indexOfDay = this.days.findIndex((element) => element.day === day);
+
+    if (this.selectedDays.includes(day)) {
+      this.selectedDays.splice(this.selectedDays.indexOf(day), 1);
+      this.days[indexOfDay].selected = false;
+    } else {
+      if (this.selectedDays.length !== this.maxSelectableDays) {
+        this.selectedDays.push(day);
+        this.days[indexOfDay].selected = true;
+      }
+    }
+
+    this.closedWithoutSelect = !!!this.selectedDays.length;
+
+    this.selectedDaysEmitter.emit({
+      selectedDays: this.selectedDays,
+      humanizedSelectedDays: this.humanizedSelectedDays,
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.range) {
+      const { initialDay, finalDay } = this.range;
+
+      for (let index = initialDay; index <= finalDay; index++) {
+        this.days.push({
+          day: index,
+          selected: false,
+          disabled: this.disabledDays.includes(index),
+        });
+      }
+    }
+  }
+}
