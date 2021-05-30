3f9f8540a949c7b4ab9f27e3ef5c2da27c8d30fe
Rafaela Queiroz <rqueiroz2108@gmail.com>
2021-05-30 18:02:14 -0300

    :sparkles: Adding event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from "@angular/core";
+import { SelectedDaysEmitter } from "./day-picker/day-picker.component";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedDays({
+    selectedDays,
+    humanizedSelectedDays,
+  }: SelectedDaysEmitter): void {
+    console.log("selectedDays", selectedDays);
+    console.log("humanizedSelectedDays", humanizedSelectedDays);
+  }
+}
